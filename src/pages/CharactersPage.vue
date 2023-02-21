58f1a4a72a96af533e06f516a67061f1bd92a1ea
Santiago Robinet <santiago.robinet@glovoapp.com>
2023-02-21 19:17:53 +0100

    Add Character type and fetch all characters

--- src/pages/CharactersPage.vue
+++ src/pages/CharactersPage.vue
@@ -1,6 +1,36 @@
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+import type { Ref } from "vue";
+import type { Character } from "../types/Character";
+
+const allCharacters: Ref<Character[]> = ref([]);
+
+async function fetchCharacters(): Promise<void> {
+  try {
+    const res = await fetch("https://rickandmortyapi.com/api/character");
+    const parsedResponse = await res.json();
+    allCharacters.value = parsedResponse.results;
+  } catch (e) {
+    alert(e);
+  }
+}
+
+onMounted(() => {
+  fetchCharacters();
+});
+</script>
+
 <template>
   <div class="characters">
-    <h1>This is an characters page</h1>
+    <h1>Characters</h1>
+    <div v-for="character in allCharacters" :key="character.id">
+      <img :src="character.image" alt="Character Image" />
+      <div>
+        <p>
+          {{ character.name }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +39,9 @@
   .characters {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: start;
   }
 }
 </style>

3719e6a221e6d938198b8ecd0c14dab98b5bf45a
Santiago Robinet <santiago.robinet@glovoapp.com>
2023-02-21 23:50:07 +0100

    install jest

--- src/pages/CharactersPage.spec.ts
+++ src/pages/CharactersPage.spec.ts
@@ -3,4 +3,7 @@ import CharactersPage from "./CharactersPage.vue";
 
 describe("CharactersPage", () => {
   const wrapper = mount(CharactersPage);
+  it("should render container", () => {
+    console.log(wrapper.html());
+  });
 });

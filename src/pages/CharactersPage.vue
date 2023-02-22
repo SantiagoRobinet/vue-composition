e961aedef7eb4ece9d9e2deefe128c5d1367dfbe
Santiago Robinet <santiago.robinet@glovoapp.com>
2023-02-22 11:30:53 +0100

    add paginations buttons styles

--- src/pages/CharactersPage.vue
+++ src/pages/CharactersPage.vue
@@ -23,9 +23,9 @@ const { charactersList, nextPage, previousPage } = useCharacters();
       />
     </div>
   </div>
-  <div>
-    <button @click="previousPage">Previous</button>
-    <button @click="nextPage">Next</button>
+  <div class="button__container">
+    <button class="button" @click="previousPage">Previous</button>
+    <button class="button" @click="nextPage">Next</button>
   </div>
 </template>
 
@@ -38,5 +38,25 @@ const { charactersList, nextPage, previousPage } = useCharacters();
 }
 .grid-item {
   padding: 16px;
+}
+
+.button__container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 12px;
+}
+.button {
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 4px;
+  margin: 0 10px;
+  font-size: 20px;
+  color: rgb(0, 255, 30);
+}
+.button:hover {
+  box-shadow: 0 0 35px 3px rgb(0, 255, 30), 0 0 50px 5px rgb(0, 255, 162);
 }
 </style>
